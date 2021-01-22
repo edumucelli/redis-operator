3df6f56d5c44b4a60387ed154a842ab589c000d7
edumucelli <edumucelli@gmail.com>
2021-01-22 13:07:42 +0000

    Improving a bit

--- src/charm.py
+++ src/charm.py
@@ -15,185 +15,144 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-from typing import Dict, List
 
+import redis
 import yaml
-from charmhelpers.core import hookenv
-from ops import model
 from ops.charm import CharmBase, ConfigChangedEvent
+from ops.framework import StoredState
 from ops.main import main
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
+
+from pod_spec import PodSpecBuilder
 
 logger = logging.getLogger(__name__)
 
-DEBUG_COUNT = 3
+REQUIRED_SETTINGS = ['image']
+
+UNIT_ACTIVE_MSG = 'Pod is ready.'
+UNIT_ACTIVE_STATUS = ActiveStatus(UNIT_ACTIVE_MSG)
+
+# We expect the redis container to use the default port
+DEFAULT_PORT = 6379
 
 
 class RedisCharm(CharmBase):
+    state = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
         self.log_debug('Initializing charm')
 
-        self.framework.observe(self.on.start, self._on_config_changed)
-        self.framework.observe(self.on.leader_elected, self._on_config_changed)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.upgrade_charm, self._on_config_changed)
-        # self.framework.observe(self.on["peer"].relation_joined, self._on_config_changed)
-        # self.framework.observe(self.on["peer"].relation_departed, self._on_config_changed)
+        # self.state.set_default(redis_initialized=False)
 
-    def _on_config_changed(self, event: ConfigChangedEvent):
-        self.log_debug("_on_config_changed event")
+        self.framework.observe(self.on.start, self.configure_pod)
+        self.framework.observe(self.on.stop, self.on_stop)
+        self.framework.observe(self.on.config_changed, self.configure_pod)
+        self.framework.observe(self.on.upgrade_charm, self.configure_pod)
+        self.framework.observe(self.on.update_status, self.on_update_status)
 
-        if self.model.unit.is_leader():
-            self._handle_leader_config_change()
-        else:
-            self._handle_non_leader_config_change()
+    def configure_pod(self, event: ConfigChangedEvent):
+        """Applies the pod configuration
+        """
+        logger.debug("Running configure_pod")
 
-    def _handle_leader_config_change(self):
-        msg = "Configuring pod"
+        if not self._is_config_valid():
+            msg = 'Charm config is not valid'
+            logger.warning(msg)
+            self.model.unit.status = BlockedStatus(msg)
+            return
+
+        if not self.model.unit.is_leader():
+            self.log_debug("Spec changes ignored by non-leader")
+            self.model.unit.status = UNIT_ACTIVE_STATUS
+            return
+
+        msg = 'Configuring pod.'
         self.log_debug(msg)
-        self.model.unit.status = model.MaintenanceStatus(msg)
+        self.model.unit.status = WaitingStatus(msg)
 
-        spec = self._make_pod_spec()
+        builder = PodSpecBuilder(
+            name=self.model.app.name,
+            port=DEFAULT_PORT,
+            image_info=self.model.config['image'],
+        )
+
+        spec = builder.build_pod_spec()
         self.log_debug(f"Pod spec:\n{yaml.dump(spec)}\n")
-        resources = self._make_pod_resources()
+
+        resources = builder.build_pod_resources()
         self.log_debug(f"Pod resources:\n{yaml.dump(resources)}\n")
         # Only the leader can set_spec().
-        self.model.pod.set_spec(spec, {"kubernetesResources": resources})
+        self.model.pod.set_spec(spec, resources)
 
-        msg = "Pod configured"
-        self.log_debug(msg)
-        self.model.unit.status = model.ActiveStatus(msg)
+        self.log_debug(UNIT_ACTIVE_MSG)
+        self.model.unit.status = UNIT_ACTIVE_STATUS
+        self.app.status = ActiveStatus('Redis pod ready.')
 
-    def _handle_non_leader_config_change(self):
-        self.log_debug("Spec changes ignored by non-leader")
-        self.model.unit.status = model.ActiveStatus()
+        logger.debug("Running configure_pod finished")
 
-    def _make_pod_spec(self) -> Dict:
-        """Set up and return our full pod spec."""
-        config_fields = {
-            "JUJU_NODE_NAME": "spec.nodeName",
-            "JUJU_POD_NAME": "metadata.name",
-            "JUJU_POD_NAMESPACE": "metadata.namespace",
-            "JUJU_POD_IP": "status.podIP",
-            # "JUJU_POD_SERVICE_ACCOUNT": "spec.serviceAccountName",
-        }
-        env_config = {k: {"field": {"path": p, "api-version": "v1"}} for k, p in config_fields.items()}
-
-        env_config["JUJU_EXPECTED_UNITS"] = " ".join(self.expected_units)
-        env_config["JUJU_APPLICATION"] = self.app.name
-
-        # vol_config = [
-        #     {"name": "charm-secrets", "mountPath": "/charm-secrets", "secret": {"name": "charm-secrets"}},
-        #     {"name": "var-run-postgresql", "mountPath": "/var/run/postgresql", "emptyDir": {"medium": "Memory"}},
-        # ]
+    def _is_config_valid(self) -> bool:
+        """Validates the charm config
+        :returns: boolean representing whether the config is valid or not.
+        """
+        logger.info('Validating charm config')
 
         config = self.model.config
+        missing = []
+        for name in REQUIRED_SETTINGS:
+            if not config.get(name):
+                missing.append(name)
 
-        spec = {
-            "version": 3,
-            "containers": [
-                {
-                    "name": self.app.name,
-                    "imageDetails": {
-                        "imagePath": config["image"],
-                    },
-                    "imagePullPolicy": "Always",  # TODO: Necessary? Should this be a Juju default?
-                    "ports": [
-                        {"name": "redis", "containerPort": 6379, "protocol": "TCP"},
-                    ],
-                    "envConfig": env_config,
-                    # "volumeConfig": vol_config,
-                    "kubernetes": {
-                        "readinessProbe": {"tcpSocket": {"port": 6379}, "initialDelaySeconds": 3, "periodSeconds": 3}
-                    },
-                }
-            ],
-            # "serviceAccount": {  # Required because we're interacting with the k8s API in this charm.
-            #     "automountServiceAccountToken": True,
-            #     "roles": [
-            #         {
-            #             "global": True,
-            #             "rules": [
-            #                 {
-            #                     "apiGroups": [""],
-            #                     "resources": ["pods"],
-            #                     "verbs": ["get", "list", "patch"],
-            #                 },
-            #             ],
-            #         },
-            #     ],
-            # },
-        }
+        if missing:
+            msg = 'Missing configuration: {}'.format(missing)
+            logger.warning(msg)
+            self.model.unit.status = BlockedStatus(msg)
+            return False
 
-        # # After logging, attach our secrets.
-        # if config.get("image_username"):
-        #     image_details["username"] = config["image_username"]
-        # if config.get("image_password"):
-        #     image_details["password"] = config["image_password"]
+        logger.info('Charm config validated')
+        return True
 
-        return spec
+    # Handles update-status event
+    def on_update_status(self, event):
+        """Set status for all units
 
-    def _make_pod_resources(self) -> Dict:
-        """Compile and return our pod resources (e.g. ingresses)."""
-        services = [
-            {
-                "name": f"{self.app.name}-master",
-                "spec": {
-                    "type": "NodePort",  # NodePort to enable external connections
-                    # We require a stable IP address selected by k8s,
-                    # so must specify the empty string for clusterIP.
-                    # The default is the string 'None', which will
-                    # give you an unstable IP address (the Pod's
-                    # internal IP I believe).
-                    "clusterIP": "",
-                    "ports": [{"name": "redis", "port": 6379, "protocol": "TCP"}],
-                    "selector": {"app.kubernetes.io/name": self.app.name, "role": "master"},
-                },
-            },
-            # {
-            #     "name": self.client_relations.standbys_service_name,
-            #     "spec": {
-            #         "type": "NodePort",  # NodePort to enable external connections
-            #         "clusterIP": "",  # A stable IP address selected by k8s.
-            #         "ports": [{"name": "pgsql", "port": 5432, "protocol": "TCP"}],
-            #         "selector": {"app.kubernetes.io/name": self.app.name, "role": "standby"},
-            #     },
-            # },
-        ]
+        Status may be
+        - [TODO] Redis API server not reachable (service is not ready),
+        - Unit is active
+        """
+        if not self.model.unit.is_leader():
+            self.model.unit.status = ActiveStatus()
+            return
 
-        resources = {
-            # "secrets": [{"name": "charm-secrets", "type": "Opaque", "data": secrets_data}],
-            # TODO: How to only make the master and standbys services
-            # externally available only after 'juju expose'?
-            "services": services,
-        }
+        # if not self.redis.is_ready():
+        #     self.unit.status = WaitingStatus('Redis not ready yet')
+        #     return
 
-        # secrets_data = {}  # Fill dictionary with secrets after logging.
-        # # Fill secrets dict with secrets.
-        # secrets = {"pgsql-admin-password": self.get_admin_password()}
-        # for k, v in secrets.items():
-        #     secrets_data[k] = b64encode(v.encode("UTF-8")).decode("UTF-8")
+        self.unit.status = ActiveStatus()
 
-        return resources
-
-    @property
-    def expected_units(self) -> List[str]:
-        # Goal state looks like this:
-        #
-        # relations: {}
-        # units:
-        #   redis/0:
-        #     since: '2020-08-31 11:05:32Z'
-        #     status: active
-        #   redis/1:
-        #     since: '2020-08-31 11:05:54Z'
-        #     status: maintenance
-        return sorted(hookenv.goal_state().get("units", {}).keys(), key=lambda x: int(x.split("/")[-1]))
+    def on_stop(self, _):
+        """Mark terminating unit as inactive
+        """
+        self.model.unit.status = MaintenanceStatus('Pod is terminating.')
 
     @staticmethod
     def log_debug(message: str):
-        logger.debug(f"[Redis {DEBUG_COUNT}] %s", message)
+        logger.debug(f"[Redis] %s", message)
+
+    @property
+    def redis(self):
+        """Return a Redis API client
+        """
+        return redis.from_url(self.redis_uri)
+
+    @property
+    def redis_uri(self):
+        """Construct a Redis URI
+        """
+        return "redis://{}:{}/".format(
+            self.model.app.name,
+            DEFAULT_PORT
+        )
 
 
 if __name__ == "__main__":
---
+++ src/pod_spec.py
@@ -0,0 +1,144 @@
+from typing import Dict, List
+from charmhelpers.core import hookenv
+
+
+class PodSpecBuilder:
+    def __init__(
+            self,
+            name: str,
+            port: int = 6379,
+            image_info: Dict = None
+    ):
+        if not image_info:
+            image_info = {}
+        self.name = name
+        self.port = port
+        self.image_info = image_info
+
+    def build_pod_spec(self) -> Dict:
+        """Set up and return our full pod spec."""
+
+        # vol_config = [
+        #     {"name": "charm-secrets", "mountPath": "/charm-secrets", "secret": {"name": "charm-secrets"}},
+        #     {"name": "var-run-postgresql", "mountPath": "/var/run/postgresql", "emptyDir": {"medium": "Memory"}},
+        # ]
+
+        spec = {
+            "version": 3,
+            "containers": [{
+                "name": self.name,
+                "imageDetails": self._build_image_spec(),
+                "imagePullPolicy": "Always",
+                "ports": self._build_port_spec(),
+                "envConfig": self._build_env_conf_spec(),
+                # "volumeConfig": vol_config,
+                "kubernetes": {
+                    "readinessProbe": self._build_readiness_spec()
+                },
+            }],
+            # "serviceAccount": {  # Required because we're interacting with the k8s API in this charm.
+            #     "automountServiceAccountToken": True,
+            #     "roles": [
+            #         {
+            #             "global": True,
+            #             "rules": [
+            #                 {
+            #                     "apiGroups": [""],
+            #                     "resources": ["pods"],
+            #                     "verbs": ["get", "list", "patch"],
+            #                 },
+            #             ],
+            #         },
+            #     ],
+            # },
+        }
+
+        # # After logging, attach our secrets.
+        # if config.get("image_username"):
+        #     image_details["username"] = config["image_username"]
+        # if config.get("image_password"):
+        #     image_details["password"] = config["image_password"]
+
+        return spec
+
+    def _build_image_spec(self):
+        return {
+            "imagePath": self.image_info,
+        }
+
+    def _build_readiness_spec(self):
+        return {
+            "tcpSocket": {
+                "port": self.port
+            },
+            "initialDelaySeconds": 10,
+            "periodSeconds": 5
+        }
+
+    def _build_port_spec(self):
+        return [{
+            "name": "redis",
+            "containerPort": self.port,
+            "protocol": "TCP"
+        }]
+
+    def _build_env_conf_spec(self) -> Dict:
+        config_fields = {
+            "JUJU_NODE_NAME": "spec.nodeName",
+            "JUJU_POD_NAME": "metadata.name",
+            "JUJU_POD_NAMESPACE": "metadata.namespace",
+            "JUJU_POD_IP": "status.podIP",
+            # "JUJU_POD_SERVICE_ACCOUNT": "spec.serviceAccountName",
+        }
+        env_config = {k: {"field": {"path": p, "api-version": "v1"}} for k, p in config_fields.items()}
+
+        env_config["JUJU_EXPECTED_UNITS"] = " ".join(self.expected_units)
+        env_config["JUJU_APPLICATION"] = self.name
+
+        return env_config
+
+    def build_pod_resources(self) -> Dict:
+        """Compile and return our pod resources (e.g. ingresses)."""
+        resources = {
+            "kubernetesResources": {
+                "services": [{
+                    "name": self.name,
+                    "spec": {
+                        "type": "NodePort",  # NodePort to enable external connections
+                        # We require a stable IP address selected by k8s,
+                        # so must specify the empty string for clusterIP.
+                        # The default is the string 'None', which will
+                        # give you an unstable IP address (the Pod's
+                        # internal IP I believe).
+                        "clusterIP": "",
+                        "ports": [
+                            {
+                                "name": "redis",
+                                "port": 6379,
+                                "protocol": "TCP"
+                            }
+                        ],
+                        "selector": {
+                            "app.kubernetes.io/name": self.name,
+                            "role": "master"
+                        },
+                    },
+                }]
+            }
+        }
+
+        return resources
+
+    @property
+    def expected_units(self) -> List[str]:
+        # Goal state looks like this:
+        #
+        # relations: {}
+        # units:
+        #   redis/0:
+        #     since: '2020-08-31 11:05:32Z'
+        #     status: active
+        #   redis/1:
+        #     since: '2020-08-31 11:05:54Z'
+        #     status: maintenance
+        return sorted(hookenv.goal_state().get("units", {}).keys(), key=lambda x: int(x.split("/")[-1]))
